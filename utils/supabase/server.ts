a6787be0502dafdcecba1dafefc44347c43a7b7c
Alexander McLean-Janet <alexander.mcleanjanet@diffuse.ly>
2025-09-15 14:45:54 +0200

    fix build

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -3,11 +3,10 @@ import { cookies } from 'next/headers'
 
 export async function createClient() {
   const cookieStore = await cookies()
-  console.log(process.env)
 
   return createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
         cookies: {
           getAll() {
