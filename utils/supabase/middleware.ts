bcac3ae4162e71ea4e3efe6c48fd29cc97186fa7
Alexander McLean-Janet <alexander.mcleanjanet@diffuse.ly>
2025-09-15 20:23:24 +0200

    fix

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -41,11 +41,14 @@ export async function updateSession(request: NextRequest) {
       !user &&
       !request.nextUrl.pathname.startsWith('/login') &&
       !request.nextUrl.pathname.startsWith('/auth') &&
-      !request.nextUrl.pathname.startsWith('/error')
+      !request.nextUrl.pathname.startsWith('/sign-in') &&
+      !request.nextUrl.pathname.startsWith('/sign-up') &&
+      !request.nextUrl.pathname.startsWith('/error') &&
+      request.nextUrl.pathname !== '/'
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
-    url.pathname = '/login'
+    url.pathname = '/sign-in'
     return NextResponse.redirect(url)
   }
 
